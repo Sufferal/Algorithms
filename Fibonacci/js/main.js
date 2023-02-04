fc8fbee93341d290cc1e8e604e95e9a30cbf14dc
Ciprian <botnari_ciprian@mail.ru>
2023-02-04 18:16:13 +0200

    Add timer

--- Fibonacci/js/main.js
+++ Fibonacci/js/main.js
@@ -1,18 +1,22 @@
+const inputElem = document.querySelector("#fibo-num");
+const startElem = document.querySelector("#start-btn");
 const btnElems = document.querySelectorAll(".algo");
 
-const toggleClass = (e) => {
-  e.target.classList.toggle("active");
-};
+// Clear input on refresh
+window.addEventListener("load", () => inputElem.value = "");
 
-const clearBtns = () => {
-  btnElems.forEach((btn) => {
-    btn.classList.remove("active");
-  });
-};
-
+// When you click on an algorithm
 btnElems.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    clearBtns();
-    toggleClass(e);
+    // Remove active from all buttons
+    btnElems.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    // Add active to the current one
+    e.target.classList.add("active");
   });
+});
+
+startElem.addEventListener("click", () => {
+  startTimer();
 });

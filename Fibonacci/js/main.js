53e1a8e8efd20291baabbd0b18d46cfa119463b7
Ciprian <botnari_ciprian@mail.ru>
2023-02-04 17:13:43 +0200

    Add algorithm buttons

--- Fibonacci/js/main.js
+++ Fibonacci/js/main.js
@@ -1,1 +1,18 @@
-console.log(1)
+const btnElems = document.querySelectorAll(".algo");
+
+const toggleClass = (e) => {
+  e.target.classList.toggle("active");
+};
+
+const clearBtns = () => {
+  btnElems.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
+
+btnElems.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    clearBtns();
+    toggleClass(e);
+  });
+});
